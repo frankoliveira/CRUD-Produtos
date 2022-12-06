4b7af5ecaefec7fb16224f9f0260af203e908030
Franklin Oliveira <defrankoliver@gmail.com>
2022-12-06 15:26:50 -0300

    Criação da view de produtos e do componente de criação de produtos. Adição das primeiras rotas.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 @NgModule({
@@ -22,7 +25,10 @@ import { HomeComponent } from './views/home/home.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
