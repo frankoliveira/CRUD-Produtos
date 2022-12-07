a0e1e316c8ef20aaebfd7c8b0f1110e32cd0d36e
Franklin Oliveira <defrankoliver@gmail.com>
2022-12-07 16:59:49 -0300

    Criados o componente de atualização de produtos.

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-update',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class ProductUpdateComponent {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
+  displayedColumns = ['id', 'name', 'price', 'action']
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    let idFromRoute = this.route.snapshot.paramMap.get('id')
+    const id = idFromRoute ? idFromRoute : ''
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto alterado!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
